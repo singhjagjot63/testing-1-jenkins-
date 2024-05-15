2542aae4c8e10dc040d95a98e8f581a396544d58
Jagjot Singh <singhjagjot63@gmail.com>
2024-05-15 21:46:07 +0530

    jenikns tutorial

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a java program");
+        System.out.println("Kidda fir sohneyo!!");
+        System.out.println(new Date());
+    }
+}
